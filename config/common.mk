390a74d31fd8da0dff9ad0291076bb86c248b3a5
mm20 <maxmustermannbsp@gmail.com>
2015-11-07 17:41:17 +0100

    Change build type to MM2.0

--- config/common.mk
+++ config/common.mk
@@ -269,7 +269,7 @@ ifdef CM_BUILDTYPE
     endif
 else
     # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
-    CM_BUILDTYPE := UNOFFICIAL
+    CM_BUILDTYPE := MM2.0
     CM_EXTRAVERSION :=
 endif
 
